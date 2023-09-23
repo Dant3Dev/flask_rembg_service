fbe6552d66757c001629533ff026a6bb96dee541
Dante Camacho <dantedev333@gmail.com>
2023-09-23 00:29:05 -0600

    Updated to use temporal files.

--- app.py
+++ app.py
@@ -1,25 +1,15 @@
-import cv2
 import os
+import tempfile
 from rembg import remove
 from PIL import Image
-from werkzeug.utils import secure_filename
-from flask import Flask,request,send_file,jsonify
+from flask import Flask,request,send_file
 from flask_cors import CORS
 
-UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg','webp'])
-
-if 'static' not in os.listdir('.'):
-    os.mkdir('static')
-
-if 'uploads' not in os.listdir('static/'):
-    os.mkdir('static/uploads')
 
 app = Flask(__name__)
 CORS(app)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key = "secret key"
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -36,14 +26,13 @@ def home():
 
 @app.route('/remback',methods=['POST'])
 def remback():
+    temp_dir = tempfile.mkdtemp()
+    output_file = os.path.join(temp_dir, 'output.png')
     file = request.files['file']
     if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        rembg_img_name = filename.split('.')[0]+"_rembg.png"
-        remove_background(UPLOAD_FOLDER+'/'+filename,UPLOAD_FOLDER+'/'+rembg_img_name)
-        procesed_image = UPLOAD_FOLDER+'/'+rembg_img_name
-        return send_file(procesed_image, mimetype='image/jpeg')
+        file.save(os.path.join(temp_dir, file.filename))
+        remove_background(os.path.join(temp_dir, file.filename), output_file)
+        return send_file(output_file, mimetype='image/jpeg')
 
 if __name__ == '__main__':
     app.run(debug=True)
